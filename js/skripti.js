6514f360f49ceabb546070f5e58b8a6ea2e78f0e
mazu0400 <matias.tolvanen13@gmail.com>
2025-03-28 12:59:06 +0200

    eka commit

---
+++ js/skripti.js
@@ -0,0 +1,28 @@
+console.log("tämä on javascript-testi.");
+alert("moro moro");
+const muutuja = "tämä on muuttuja";
+
+function tsti() {
+    let p = 5*6;
+    let x, y, z;
+    (x = 5) , (y = 2)
+    z = x + y;
+
+    console.log("this is  z: ", z);
+    let toinen = "tänne tulee arvo";
+    console.log(toinen)
+}
+
+//kutsu funkto näin:
+tsti();
+
+
+function testi(){
+    let x=7;
+    let y=2;
+    let z=0;
+     x+y-z;
+    console.log("tulos on z:",z);
+}
+testi();
+
